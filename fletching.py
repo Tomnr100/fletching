62b81a45cc3d312a16771af79edff4910aab9732
tomnr100 <fatalitytom@hotmail.com>
2020-12-16 15:41:37 +0100

    fletching

--- fletching.py
+++ fletching.py
@@ -12,8 +12,8 @@ def banking():
     chestdimensions = pyautogui.locateOnScreen('E:\\Desktop\\pycodes - kopie\\fletching\\bankchest.png', confidence=0.8)
     print(chestdimensions)
     chestX = random.uniform(chestdimensions[0]+(0.1*chestdimensions[2]), (chestdimensions[0]+(chestdimensions[2])*0.9))
-    chestY = random.uniform(chestdimensions[1]+(0.1*chestdimensions[2]), (chestdimensions[1]+(chestdimensions[3])*0.9))
-    dragdur = random.normalvariate(1, 0.5)
+    chestY = random.uniform(chestdimensions[1]+(0.1*chestdimensions[3]), (chestdimensions[1]+(chestdimensions[3])*0.9))
+    dragdur = random.normalvariate(1, 0.2)
     pyautogui.moveTo(chestX, chestY, dragdur)
     pyautogui.click()
     time.sleep(randsleep)
@@ -22,12 +22,11 @@ def banking():
 def depositall():
     print("Depositing items...")
     depositdimensions = pyautogui.locateOnScreen(
-        'E:\\Desktop\\pycodes - kopie\\fletching\\depositall.png')
-    depositx = random.uniform(
-        depositdimensions[0], depositdimensions[0]+(depositdimensions[2])*0.9)
-    deposity = random.uniform(
-        depositdimensions[1], depositdimensions[1]+(depositdimensions[3])*0.9)
-    dragdur = random.normalvariate(1, 0.5)
+        'E:\\Desktop\\pycodes - kopie\\fletching\\depositall.png', confidence=0.9)
+    print(depositdimensions)
+    depositx = random.uniform(depositdimensions[0]+(0.1*depositdimensions[2]), depositdimensions[0]+(depositdimensions[2])*0.9)
+    deposity = random.uniform(depositdimensions[1]+(0.1*depositdimensions[3]), depositdimensions[1]+(depositdimensions[3])*0.9)
+    dragdur = random.normalvariate(1, 0.2)
     pyautogui.moveTo(depositx, deposity, dragdur)
     pyautogui.click()
     time.sleep(randsleep)
@@ -38,10 +37,10 @@ def withdrawlogs():
     logdimensions = pyautogui.locateOnScreen(
         f'E:\\Desktop\\pycodes - kopie\\fletching\\{logselection}.png', confidence=0.9)
     logx = random.uniform(
-        logdimensions[0], logdimensions[0]+(logdimensions[2])*0.9)
+        logdimensions[0]+(logdimensions[2]*0.1), logdimensions[0]+(logdimensions[2])*0.9)
     logy = random.uniform(
-        logdimensions[1], logdimensions[1]+(logdimensions[3])*0.9)
-    dragdur = random.normalvariate(1, 0.5)
+        logdimensions[1]+(logdimensions[3]*0.1), logdimensions[1]+(logdimensions[3])*0.9)
+    dragdur = random.normalvariate(1, 0.2)
     pyautogui.moveTo(logx, logy, dragdur)
     pyautogui.click()
     time.sleep(randsleep)
@@ -54,10 +53,10 @@ def startfletch():
     knifedimensions = pyautogui.locateOnScreen(
         'E:\\Desktop\\pycodes - kopie\\fletching\\invknife.png')
     knifex = random.uniform(
-        knifedimensions[0], knifedimensions[0]+(knifedimensions[2])*0.9)
+        knifedimensions[0]+(0.1*knifedimensions[2]), knifedimensions[0]+(knifedimensions[2])*0.9)
     knifey = random.uniform(
-        knifedimensions[1], knifedimensions[1]+(knifedimensions[3])*0.9)
-    dragdur = random.normalvariate(1, 0.5)
+        knifedimensions[1]+(0.1*knifedimensions[3]), knifedimensions[1]+(knifedimensions[3])*0.9)
+    dragdur = random.normalvariate(1, 0.2)
     pyautogui.moveTo(knifex, knifey, dragdur)
     pyautogui.click()
     time.sleep(randsleep)
@@ -66,13 +65,13 @@ def startfletch():
     logdimensions = pyautogui.locateOnScreen(
         f'E:\\Desktop\\pycodes - kopie\\fletching\\inv{logselection}.png', confidence=0.9)
     logx = random.uniform(
-        logdimensions[0], logdimensions[0]+(logdimensions[2])*0.9)
+        logdimensions[0]+(logdimensions[2]*0.1), logdimensions[0]+(logdimensions[2])*0.9)
     logy = random.uniform(
-        logdimensions[1], logdimensions[1]+(logdimensions[3])*0.9)
-    dragdur = random.normalvariate(1, 0.5)
+        logdimensions[1]+(logdimensions[3]*0.1), logdimensions[1]+(logdimensions[3])*0.9)
+    dragdur = random.normalvariate(1, 0.2)
     pyautogui.moveTo(logx, logy, dragdur)
     pyautogui.click()
-    time.sleep(randsleep)
+    time.sleep(randsleep+0.5)
 
     #confirm last made object
     pyautogui.hotkey('space')
@@ -80,8 +79,16 @@ def startfletch():
     print("Sleeping...")
     time.sleep(random.uniform(50, 54))
 
-for i in range(2):
-    banking()
-    depositall()
-    withdrawlogs()
-    startfletch()
+def startbot():
+    try:
+        banking()
+        depositall()
+        withdrawlogs()
+        startfletch()
+    except:
+        pyautogui.hotkey('esc')
+        return
+
+
+while True:
+    startbot()
